86dab10172b4996ea8b21fbee1e1179ddae9bc18
aadilshk620 <aadilshaikh620@gmail.com>
2022-04-27 00:32:20 +0530

    fist code

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+public class helloworld {
+    public static void main (String [] arg){
+        System.out.println("hellow world");
+    }
+}
+
+public class cal{
+    public static void main(String [] args){
+        
+    }
+}
